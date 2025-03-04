6184838f8d6d33f1dd3a2c671558deeb1fffba47
Hugo Soares <hugoferreirasoares@gmail.com>
2025-03-04 18:48:41 +0000

    feat: SO esqueleto done

---
+++ LEI 2ano/2 Semestre/SO/src/esqueleto-ex5_6/searchM.c
@@ -0,0 +1,56 @@
+#include "matrix.h"
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <sys/types.h>
+
+#define NOT_FOUND 255
+
+
+int main(int argc, char *argv[]) {
+
+    // generate random matrix
+    int **matrix = createMatrix();
+
+    // print matrix
+    printMatrix(matrix);
+
+    int value = atoi(argv[1]);
+    for (int i = 0; i < ROWS; i++)
+    {
+        pid_t pid = fork();
+        if (pid < 0) { 
+            perror("On fork"); 
+            exit(1);
+        }
+        else if (pid == 0) {
+            for (int j = 0; j < COLUMNS; j++)
+            {
+                if (matrix[i][j] == value) _exit(i);
+            }
+            _exit(NOT_FOUND); 
+        }
+    }
+    for (int i = 0; i < ROWS; i++)
+    {
+        int status;
+        wait(&status);
+        if (WIFEXITED(status)) {
+            int result = WEXITSTATUS(status);
+            if (result < ROWS) {
+                printf("Found %d in line %d\n", value, result);
+            }
+        }
+    }
+    
+    
+    
+
+    // free matrix
+    for (int i = 0; i < ROWS; i++) {
+        free(matrix[i]);
+    }
+    free(matrix);
+
+    return 0;
+}
